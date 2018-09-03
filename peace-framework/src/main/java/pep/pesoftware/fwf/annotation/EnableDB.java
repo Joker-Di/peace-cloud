9dfe8396adc7fea015abaf01e6cd39f0d304a2b5
Dong <1561826175@qq.com>
2018-09-03 11:38:24 +0800

    初始化项目配置

--- peace-framework/src/main/java/pep/pesoftware/fwf/annotation/EnableDB.java
+++ peace-framework/src/main/java/pep/pesoftware/fwf/annotation/EnableDB.java
@@ -7,6 +7,6 @@ import java.lang.annotation.*;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@MapperScan(basePackages = "com.homie.app.framework.repository.mapper")
+@MapperScan(basePackages = "pep.pesoftware.fwf.repository.mapper")
 public @interface EnableDB {
 }
