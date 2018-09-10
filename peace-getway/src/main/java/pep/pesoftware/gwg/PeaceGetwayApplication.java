844309b00b6f3d84f71df7a10abd2dc4a002cda4
Dong <1561826175@qq.com>
2018-09-10 14:39:03 +0800

    初始化项目配置

--- peace-getway/src/main/java/pep/pesoftware/gwg/PeaceGetwayApplication.java
+++ peace-getway/src/main/java/pep/pesoftware/gwg/PeaceGetwayApplication.java
@@ -4,7 +4,7 @@ package pep.pesoftware.gwg;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +14,7 @@ import org.springframework.web.filter.CorsFilter;
 import pep.pesoftware.gwg.filter.GetWayFilter;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableZuulProxy
 @EnableFeignClients
 @EnableCircuitBreaker
--- peace-web/src/main/java/pep/pesoftware/wew/aop/GlobalExceptionHandler.java
+++ peace-web/src/main/java/pep/pesoftware/wew/aop/GlobalExceptionHandler.java
@@ -1,23 +1,32 @@
 package pep.pesoftware.wew.aop;
 
-//@ControllerAdvice
-//@Slf4j
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import pep.pesoftware.wew.exception.BaseException;
+import pep.pesoftware.wew.form.BaseForm;
+
+import javax.servlet.http.HttpServletResponse;
+
+@ControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
-//    @ExceptionHandler(value = Exception.class)
-//    @ResponseBody
-//    public BaseForm handleException(Exception exception, HttpServletResponse response){
-//        if (exception instanceof BaseException){
-//            BaseException baseException = (BaseException) exception;
-//            int httpStatusCode = baseException.getHttpStatus().value();
-//            String errorCode = baseException.getCode();
-//            String errorMsg = baseException.getMessage();
-//            response.setStatus(httpStatusCode);
-//            return new BaseForm(errorCode,errorMsg);
-//        }else{
-//            log.error("系统异常",exception);
-//            return new BaseForm("E001","系统异常");
-//        }
-//    }
+    @ExceptionHandler(value = Exception.class)
+    @ResponseBody
+    public BaseForm handleException(Exception exception, HttpServletResponse response){
+        if (exception instanceof BaseException){
+            BaseException baseException = (BaseException) exception;
+            int httpStatusCode = baseException.getHttpStatus().value();
+            String errorCode = baseException.getCode();
+            String errorMsg = baseException.getMessage();
+            response.setStatus(httpStatusCode);
+            return new BaseForm(errorCode,errorMsg);
+        }else{
+            log.error("系统异常",exception);
+            return new BaseForm("E001","系统异常");
+        }
+    }
 
 }
